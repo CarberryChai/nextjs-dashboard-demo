1372531b85890501e3bd85f1b86f30f2b833a552
Changlin <CarberryChai@gmail.com>
2023-12-16 20:01:34 +0800

    streaming

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,5 @@
+// create a fallback ui as a replacement while page is loading
+import DashboardSkeleton from "@/app/ui/skeletons"
+export default function Loading() {
+  return <DashboardSkeleton />
+}
